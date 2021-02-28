75a9f9401e796e85d26bea48f036c49c85bf1a02
Joash Christian Liwanag <nightlock0326@gmail.com>
2021-03-01 01:15:30 +0800

    Add decoding to file

--- src/decode.rs
+++ src/decode.rs
@@ -1,6 +1,7 @@
 use image;
 use rqrr;
 use rqrr::MetaData;
+use std::fs;
 fn decoder(path: String) -> (MetaData, String) {
     // Load on image to search, convert it to grayscale
     let img = image::open(path).unwrap().to_luma8();
@@ -18,4 +19,10 @@ pub fn decode_img(path: String) {
     let content = img_content.1;
 
     println!("{}", content);
+}
+
+pub fn decode_to_file(path: String, file_name: String) {
+    let img_content = decoder(path);
+    let content = img_content.1;
+    fs::write(file_name, content).expect("Failed to write file");
 }
--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,13 @@ fn main() {
     let args = App::from(yaml).get_matches();
 
     if let Some(ref args) = args.subcommand_matches("decode") {
-        if args.is_present("input") {
+        if args.is_present("input") && args.is_present("output") == false {
             let path = args.value_of("input").unwrap().to_string();
             decode::decode_img(path);
+        } else if args.is_present("output") && args.is_present("input") {
+            let path = args.value_of("input").unwrap().to_string();
+            let file_name = args.value_of("output").unwrap().to_string();
+            decode::decode_to_file(path, file_name);
         }
     }
     if let Some(ref args) = args.subcommand_matches("encode") {
@@ -20,16 +24,13 @@ fn main() {
             let message = args.value_of("input").unwrap().to_string();
             let file_name = args.value_of("output").unwrap().to_string();
             encode::encode_img(message, file_name);
-        }
-        if (args.is_present("input") || args.is_present("from-file"))
+        } else if (args.is_present("input") || args.is_present("from-file"))
             && args.is_present("output") == false
         {
             println!("Please provide a file name for the output.");
-        }
-        if args.is_present("input") == false && args.is_present("from-file") == false {
+        } else if args.is_present("input") == false && args.is_present("from-file") == false {
             println!("Please provide an input to encode.");
-        }
-        if args.is_present("from-file")
+        } else if args.is_present("from-file")
             && args.is_present("output")
             && args.is_present("input") == false
         {
