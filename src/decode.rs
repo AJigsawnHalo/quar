8ef302cb707d1d9599b7ac5cfd9b1007d38f910c
Joash Liwanag <nightlock0326@gmail.com>
2021-03-07 16:07:37 +0800

    Add ECC Options (#2)
    * Add visible aliases for subcommands
    
    * Add Install Instructions
    
    * Add option to encode/decode QR codes with ECC.

--- src/decode.rs
+++ src/decode.rs
@@ -5,27 +5,28 @@ use std::fs::OpenOptions;
 use std::io::Write;
 
 fn decoder(path: String) -> (MetaData, String) {
-    // Load on image to search, convert it to grayscale
     let img = image::open(path).unwrap().to_luma8();
-    // Prepare for detection
     let mut img = rqrr::PreparedImage::prepare(img);
-    // Search for grids, without decoding
     let grids = img.detect_grids();
-    // Decode the grid
     let (meta, content) = grids[0].decode().unwrap();
     (meta, content)
 }
 
-pub fn decode_img(path: String) {
-    let img_content = decoder(path);
-    let content = img_content.1;
+pub fn decode_img(path: String, ret_ecc: bool) {
+    let (meta, content) = decoder(path);
+    let content = content;
 
-    println!("{}", content);
+    if ret_ecc == true {
+        let ecc_lvl = return_ecc(meta);
+        println!("Error Correction Level: {}", ecc_lvl);
+    }
+
+    println!("Data: {}", content);
 }
 
-pub fn decode_to_file(path: String, file_name: String) {
-    let img_content = decoder(path);
-    let content = img_content.1;
+pub fn decode_to_file(path: String, file_name: String, ret_ecc: bool) {
+    let (meta, content) = decoder(path);
+    let content = content;
     let mut file = OpenOptions::new()
         .write(true)
         .append(true)
@@ -33,5 +34,14 @@ pub fn decode_to_file(path: String, file_name: String) {
         .open(file_name)
         .expect("Unable to open file.");
 
+    if ret_ecc == true {
+        let ecc_lvl = return_ecc(meta);
+        writeln!(&file, "ECC Level: {}", ecc_lvl).expect("Unable to write to file.")
+    }
     writeln!(file, "{}", content).expect("Unable to write file.");
+}
+
+fn return_ecc(meta: MetaData) -> u16 {
+    let ecc_lvl = meta.ecc_level;
+    ecc_lvl
 }
--- src/main.rs
+++ src/main.rs
@@ -9,17 +9,26 @@ fn main() {
     if let Some(ref args) = args.subcommand_matches("decode") {
         if args.is_present("input") && args.is_present("output") == false {
             let path = args.value_of("input").unwrap().to_string();
-            decode::decode_img(path);
+            if args.is_present("ecc-level") {
+                decode::decode_img(path, true);
+            } else {
+                decode::decode_img(path, false);
+            }
         } else if args.is_present("output") && args.is_present("input") {
             let path = args.value_of("input").unwrap().to_string();
             let file_name = args.value_of("output").unwrap().to_string();
-            decode::decode_to_file(path, file_name);
+            if args.is_present("ecc-level") {
+                decode::decode_to_file(path, file_name, true);
+            } else {
+                decode::decode_to_file(path, file_name, false);
+            }
         }
     }
     if let Some(ref args) = args.subcommand_matches("encode") {
         if args.is_present("input")
             && args.is_present("output")
             && args.is_present("from-file") == false
+            && args.is_present("ecc-level") == false
         {
             let message = args.value_of("input").unwrap().to_string();
             let file_name = args.value_of("output").unwrap().to_string();
@@ -33,10 +42,29 @@ fn main() {
         } else if args.is_present("from-file")
             && args.is_present("output")
             && args.is_present("input") == false
+            && args.is_present("ecc-level") == false
         {
             let path = args.value_of("from-file").unwrap().to_string();
             let file_name = args.value_of("output").unwrap().to_string();
             encode::encode_from_file(path, file_name);
+        } else if (args.is_present("input") || args.is_present("from-file"))
+            && args.is_present("output")
+            && args.is_present("ecc-level")
+        {
+            let file_name = args.value_of("output").unwrap().to_string();
+            let ecc_lvl = args.value_of("ecc-level").unwrap().parse::<u16>().unwrap();
+            println!("{}", &ecc_lvl);
+            if ecc_lvl >= 4 {
+                println!("Invalid ECC level. Possible values are '0', '1', '2', '3'.");
+            } else {
+                if args.is_present("input") {
+                    let message = args.value_of("input").unwrap().to_string();
+                    encode::encode_img_ecc(message, file_name, ecc_lvl);
+                } else if args.is_present("from-file") {
+                    let path = args.value_of("from-file").unwrap().to_string();
+                    encode::encode_from_file_ecc(path, file_name, ecc_lvl);
+                }
+            }
         }
     }
 }
